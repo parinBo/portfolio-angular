88a3164d7517f4f8b30d7b5a0bc7b711d075819d
parin <bpyr.wk@gmail.com>
2022-04-10 16:43:20 +0700

    upd photo img

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,18 +27,19 @@ export class AppComponent {
       const sec1 = document.querySelector('#sec1');
       const sec2 = document.querySelector('#sec2');
       const sec3 = document.querySelector('#sec3');
+      console.log(window.pageYOffset,sec2!.clientHeight,sec2!.scrollHeight)
+
       // if (window.pageYOffset < 300 ) {
       //   document.querySelector('.fade-X')?.classList.add('is-visible')
       // } else {
       //   document.querySelector('.fade-X')?.classList.remove('is-visible')
       // }
-      if (window.pageYOffset > 300 && window.pageYOffset < 1000 ) {
-        console.log('ok2',window.pageYOffset,sec2!.clientHeight,sec2!.scrollHeight)
+      if (window.pageYOffset > 300 && window.pageYOffset < 700 ) {
         sec2!.classList.add('is-visible');
       } else {
         sec2!.classList.remove('is-visible');
       }
-      if (window.pageYOffset > 600 ) {
+      if (window.pageYOffset > 670 ) {
         sec3!.classList.add('is-visible');
       } else {
         sec3!.classList.remove('is-visible');
@@ -46,4 +47,7 @@ export class AppComponent {
     });
   }
 
+  sec(){
+    document.querySelector('#sec4')?.scrollIntoView()
+  }
 }
